aee6070498d283d321ce48b3c8652e96082ef999
eyaler <eyalgruss@gmail.com>
2019-09-24 01:24:05 +0300

    some more chord cleanup

--- stats.py
+++ stats.py
@@ -16,7 +16,7 @@ fold_inversions = True
 # ('madd9', (0, 3, 7, 14))
 
 def fix(chord):
-    chord = chord.replace('ADD', 'add').replace('7b', '7-').replace('7b', '7-').replace('7#', '7+').replace('dim7', 'dim6').replace('maj7', 'M7').replace('maj9', 'M9').replace('m11', '11').replace('(', '').replace(')', '')
+    chord = chord.replace('ADD', 'add').replace('7b', '7-').replace('7b', '7-').replace('7#', '7+').replace('dim7', 'dim6').replace('maj7', 'M7').replace('maj9', 'M9').replace('m11', '11').replace('B#','C').replace('E#','F').replace('Cb','B').replace('Fb','E').replace('(', '').replace(')', '')
     if chord.endswith('7sus'):
         chord = chord.replace('7sus', '7sus4')
     elif chord.endswith('sus'):
